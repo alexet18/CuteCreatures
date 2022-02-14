322fc4dec2d347b044c402fb480ecdb67563c084
Tudor Alexe <tudor@localhost.localdomain>
2022-02-14 23:20:10 +0200

    init

---
+++ src/app/components/creature-types/creature-types.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-creature-types',
+  templateUrl: './creature-types.component.html',
+  styleUrls: ['./creature-types.component.css']
+})
+export class CreatureTypesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/components/rarities/rarities.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-rarities',
+  templateUrl: './rarities.component.html',
+  styleUrls: ['./rarities.component.css']
+})
+export class RaritiesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
